351a6bb4557f7fbce0bdae2e220f62494bade3d7
Génesis Falcón <genesisfalcon15@gmail.com>
2026-04-10 23:29:32 +0200

    mapa hecho

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Buscador } from "./pages/Buscador.jsx";
+
 
 
 
@@ -8,6 +10,7 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<Home />} path="/" />
+      <Route element={<Buscador />} path="/Buscador" />
       <Route element={<h1>Not found!</h1>} path="*" />
     </Routes>
   );
