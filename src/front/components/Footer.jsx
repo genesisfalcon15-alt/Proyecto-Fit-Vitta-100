83b350e8d60c39872e096bb4323b9cf3ccbcbde9
Génesis Falcón <genesisfalcon15@gmail.com>
2026-04-10 15:43:41 +0200

    cambio a las 15:45

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
-=======
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
@@ -24,7 +11,7 @@ export const Footer = () => {
 			style={{ height: "70px", zIndex: 1000, maxWidth: "450px", left: "50%", transform: "translateX(-50%)" }}>
 
 
-			{/* creando el boton home*/}
+
 
 			<Link to="/" className="text-decoration-none text-center">
 				<div className="d-flex flex-column align-items-center">
@@ -34,7 +21,7 @@ export const Footer = () => {
 			</Link>
 
 
-			{/* creando el boton mapa&buscador */}
+
 			<Link to="/buscador" className="text-decoration-none text-center">
 				<div className="d-flex flex-column align-items-center">
 					<i className="fas fa-map-marker-alt" style={{ fontSize: "20px", color: isActive("/buscador") }}></i>
@@ -43,14 +30,13 @@ export const Footer = () => {
 			</Link>
 
 
-			{/*boton IMC*/}
+
 			<Link to="/imc" className="text-decoration-none text-center">
 				<div className="d-flex flex-column align-items-center">
 					<i className="fas fa-user-circle" style={{ fontSize: "20px", color: isActive("/imc") }}></i>
-					<span style={{ fontSize: "12px", color: isActive("/buscador"), fontWeight: "600" }}>Mapa</span>
+					<span style={{ fontSize: "12px", color: isActive("/buscador"), fontWeight: "600" }}>Usuario</span>
 				</div>
 			</Link>
 		</footer>
 	);
 }
->>>>>>> e9cebee (cambios en el diseño y nuevos componentes)
