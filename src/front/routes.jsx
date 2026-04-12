b9abf88240d8e0dc42fb5aec060236d2e7b6868e
Génesis Falcón <genesisfalcon15@gmail.com>
2026-04-12 18:49:34 +0200

    terminado la primera tarea

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Buscador } from "./pages/Buscador.jsx";
-
+import { Imc } from "./pages/Imc.jsx";
+import Signup from "./pages/Signup.jsx";
+import Signin from "./pages/Signin.jsx";
 
 
 
@@ -11,8 +13,9 @@ export const AppRoutes = () => {
     <Routes>
       <Route element={<Home />} path="/" />
       <Route element={<Buscador />} path="/Buscador" />
-         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/Imc" element={<Imc />} />
       <Route element={<h1>Not found!</h1>} path="*" />
     </Routes>
   );
