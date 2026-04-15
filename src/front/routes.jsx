995b346529ebe7a40d729a753931d07573f38eeb
sergio19000 <119664418+sergio19000@users.noreply.github.com>
2026-04-15 17:09:59 +0000

    cambios cloudinary y back end

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -6,6 +6,7 @@ import { Imc } from "./pages/Imc.jsx";
 import Signup from "./pages/Signup.jsx";
 import Signin from "./pages/Signin.jsx";
 import Conocenos from "./pages/Conocenos.jsx";
+import Lista from "./pages/Lista.jsx";
 
 
 
