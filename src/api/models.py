710e11cea48ae2c72a575b6b4b9a568651286506
patrcarrasco <pat.r.carrasco@gmail.com>
2026-04-20 11:15:01 +0200

    Product Model añadido

--- src/api/models.py
+++ src/api/models.py
@@ -62,3 +62,23 @@ class HistorialPeso(db.Model):
             "altura": self.altura,
             "fecha": self.fecha.isoformat()
         }
+
+class Product(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    store: Mapped[str] = mapped_column(String(120), nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    category: Mapped[str] = mapped_column(String(50), nullable=False)
+    image: Mapped[str] = mapped_column(String(300), nullable=True)
+    added: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "store": self.store,
+            "price": self.price,
+            "category": self.category,
+            "image": self.image,
+            "added": self.added
+}
